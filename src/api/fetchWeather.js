af3f3a13d9bff287b5b6a333b0d3101a90b2bc4d
Samuel Jo <jo.sunghak@gmail.com>
2021-07-12 15:56:41 -0400

    Initialized envvar

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,17 +1,10 @@
 import axios from 'axios'
 import React, {useState} from 'react';
 
-const URL = 'http://api.openweathermap.org/data/2.5/weather';
-const API_KEY = '834a2ed01ae2d2bd74bb30f9fd76dd12'
+const URL = process.env.REACT_APP_API_URL;
+const API_KEY = process.env.REACT_APP_API_KEY
 
 export const fetchWeather = async (query) =>{
-  // const {data} = await axios.get(`${process.env.API_URL}`, {
-  //   params:{
-  //     q: query,
-  //     units: 'metric',
-  //     APPID: `${process.env.API_KEY}`
-  //   }
-  // })
   const {data} = await axios.get(URL, {
     params:{
       q: query,
